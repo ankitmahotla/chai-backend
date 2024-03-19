8e1c3d64b14eb1d8aa24758bc63c31cd324e194a
Ankit Mahotla <ankitmahotla45@gmail.com>
2024-03-19 11:49:20 +0530

    add comment, like, tweet, playlist models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -136,8 +136,8 @@ const loginUser = asyncHandler(async (req, res) => {
 
 const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(req.user._id, {
-    $set: {
-      refreshToken: undefined
+    $unset: {
+      refreshToken: 1 //removes the field from document
     }
   },
     {
